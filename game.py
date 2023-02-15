c163e60cf710418137d19b30892766c253187d39
unknown <mohammed-el-mehdi.alaoui@imt-atlantique.fr>
2023-02-15 18:02:01 +0100

    new file

---
+++ game.py
@@ -0,0 +1,31 @@
+import pygame
+import sys
+
+from level import Level
+from settings import *
+from debug import debug
+
+
+
+class Game:
+    def __init__(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
+        self.clock = pygame.time.Clock()
+        pygame.display.set_caption("outlands")
+        self.level = Level()
+
+    def run(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+            self.screen.fill('black')
+            self.clock.tick(FPS)
+            self.level.run()
+            pygame.display.update()
+
+
+game = Game()
+game.run()
---
+++ settings.py
@@ -0,0 +1,46 @@
+FPS = 60
+HEIGHT = 1000
+WIDTH = 800
+TILESIZE = 64
+
+weapon_data = {
+    'sword' : {'cooldown':100,'damage':15,'graphic':'NinjaAdventure/Items/Weapons/Sword/Sprite.png'},
+    'lance' : {'cooldown':250,'damage':30,'graphic':'NinjaAdventure/Items/Weapons/Lance2/Sprite.png'}
+}
+
+lvl1_obj = {
+    'coin' : {'type':'auto_collect', 'graphic':'Graphics/coin.jpg' },
+    'weapon' : {'type':'weapon','graphic':'NinjaAdventure/Items/Weapons/Lance2/Sprite.png'}
+
+}
+
+# ui
+BAR_HEIGHT = 20
+HEALTH_BAR_WIDTH = 200
+ENERGY_BAR_WIDTH = 140
+ITEM_BOX_SIZE = 80
+UI_FONT = 'public-pixel-font/PublicPixel-z84yD.ttf'
+UI_FONT_SIZE = 18
+
+# general colors
+WATER_COLOR = '#71ddee'
+UI_BG_COLOR = '#222222'
+UI_BORDER_COLOR = '#111111'
+TEXT_COLOR = '#EEEEEE'
+
+# ui colors
+HEALTH_COLOR = 'red'
+ENERGY_COLOR = 'blue'
+UI_BORDER_COLOR_ACTIVE = 'gold'
+
+# magic
+magic_data = {
+	'flame': {'strength': 5,'cost': 20,'graphic':'Graphics/flame/fire.png'},
+	'heal' : {'strength': 20,'cost': 10,'graphic':'Graphics/heal/heal.png'}}
+
+# enemy
+monster_data = {
+	'squid': {'health': 100,'exp':100,'damage':20,'attack_type': 'slash', 'attack_sound':'../audio/attack/slash.wav', 'speed': 3, 'resistance': 3, 'attack_radius': 80, 'notice_radius': 360},
+	'raccoon': {'health': 300,'exp':250,'damage':40,'attack_type': 'claw',  'attack_sound':'../audio/attack/claw.wav','speed': 2, 'resistance': 3, 'attack_radius': 120, 'notice_radius': 400},
+	'spirit': {'health': 100,'exp':110,'damage':8,'attack_type': 'thunder', 'attack_sound':'../audio/attack/fireball.wav', 'speed': 4, 'resistance': 3, 'attack_radius': 60, 'notice_radius': 350},
+	'bamboo': {'health': 70,'exp':120,'damage':6,'attack_type': 'leaf_attack', 'attack_sound':'../audio/attack/slash.wav', 'speed': 3, 'resistance': 3, 'attack_radius': 50, 'notice_radius': 300}}
